507dd97a3e4cb7c00a89ac8b3bb2f1bb7ca2e641
BeauTaapken <beautaapken@me.com>
2019-11-12 17:53:23 +0100

    * Changed websocket to accept all url's

--- src/main/java/anime/chat/animechat/config/WebSocketConfig.java
+++ src/main/java/anime/chat/animechat/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.socket.config.annotation.WebSocketTransportRegist
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/camera").setAllowedOrigins("http://localhost:8080").withSockJS();
+        registry.addEndpoint("/camera").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
